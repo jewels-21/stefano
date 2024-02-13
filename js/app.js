e449697aee2d46342495ef3f0ff8633b8fa60339
jewels21 <laza81@libero.it>
2024-02-13 23:13:13 +0100

    update lavori

--- js/app.js
+++ js/app.js
@@ -13,3 +13,51 @@ $('#return-to-top').click(function () {      // When arrow is clicked
         scrollTop: 0                       // Scroll to top of body
     }, 500);
 });
+
+
+    // Define slideIndex globally
+    var slideIndex = {};
+
+    // Function to open the modal and start the slideshow for a given modal ID
+    function openAndShowModal(modalId) {
+        // Open the modal
+        $('#' + modalId).foundation('open');
+
+        // Start slideshow for the given modal ID
+        showSlides(1, modalId);
+
+        // Bind a listener to the modal close event
+        $('#' + modalId).on('closed.zf.reveal', function() {
+            // Reset slideIndex for the modal
+            slideIndex[modalId] = 1;
+        });
+    }
+
+    // Function to close the modal and reset slideIndex for the modal
+    function closeModalAndReset(modalId) {
+        // Close the modal
+        $('#' + modalId).foundation('close');
+
+        // Reset slideIndex for the modal
+        slideIndex[modalId] = 1;
+    }
+
+    // Next/previous controls for slideshow
+    function plusSlides(n, modalId) {
+        showSlides(slideIndex[modalId] += n, modalId);
+    }
+
+    // Display the slides for a given modal ID
+    function showSlides(n, modalId) {
+        var i;
+        var slides = document.querySelectorAll('#' + modalId + ' .slideshow-img');
+        if (!slideIndex[modalId]) {
+            slideIndex[modalId] = 1;
+        }
+        if (n > slides.length) { slideIndex[modalId] = 1; }
+        if (n < 1) { slideIndex[modalId] = slides.length; }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex[modalId] - 1].style.display = "block";
+    }
